c00eecc922d5ce4050c36c20f279446f970685e4
vivianafb <vivianafb@gmail.com>
2018-10-15 01:52:27 -0300

    Terminado

---
+++ 1 metodos/Ejercicio1.rb
@@ -0,0 +1,16 @@
+# Optimiza el Codigo utilizando tu editor de texto.
+# hint: Debes utilizar 'rubocop'
+# y no obtener errores ni warnings.
+
+def par(num)
+  if (num % 2).zero?
+    true
+  else
+    false
+  end
+end
+
+puts par(2)
+puts par(3)
+puts par(4)
+puts par(5)
